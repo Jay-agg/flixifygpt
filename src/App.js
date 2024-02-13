0ad27599b156f8293426a09413835f51160fce94
Jayant Aggarwal <jadps2004@gmail.com>
2024-02-14 03:00:50 +0530

    title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    document.title = "NetflixGPT";
-  }, []);
   return (
     <Provider store={appStore}>
       <Body />
